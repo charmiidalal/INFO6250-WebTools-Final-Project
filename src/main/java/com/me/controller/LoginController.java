2b659064eeb768a6054aa4b07471acddf807768f
charmiidalal <dalal.c@northeastern.edu>
2021-04-17 02:10:14 -0400

    Add signup api

--- src/main/java/com/me/controller/LoginController.java
+++ src/main/java/com/me/controller/LoginController.java
@@ -1,11 +1,12 @@
 package com.me.controller;
 
+import io.jsonwebtoken.Jwts;
+import com.google.gson.Gson;
 import com.me.dao.UserDao;
 import com.me.pojo.User;
+import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.Response;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,21 +14,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
 //    @PostMapping("user/main.htm")
-    @RequestMapping(value = "user/main.htm", method = RequestMethod.POST)
+    @RequestMapping(value = "/login.htm", method = RequestMethod.POST)
     public ResponseEntity<String> UserLogin(@RequestParam("username") String username, @RequestParam("password") String password,
             HttpServletRequest request, HttpServletResponse response) {
         UserDao ad = new UserDao();
 //        HttpHeaders headers = new HttpHeaders();
         HttpHeaders headers = new HttpHeaders();
+
         headers.add("Custom-Header", "foo");
+        Gson gson = new Gson();
+
         User uap = ad.getUser(username);
         if (uap != null) {
             String resultpassword = uap.getPassword();
@@ -35,7 +36,7 @@ public class LoginController {
 
             if (username.equals(resultusername) && password.equals(resultpassword)) {
                 return new ResponseEntity<>(
-                        "Auth", headers, HttpStatus.OK);
+                        gson.toJson("{accessToken:" + getJwt(username)) + "}", headers, HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(
                         "Unauthorized", headers, HttpStatus.UNAUTHORIZED);
@@ -53,6 +54,17 @@ public class LoginController {
 //            tx.commit();
 //        }
 
+    }
+
+    public String getJwt(String username) {
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        String jwtToken = Jwts.builder()
+                .claim("username", username)
+                .claim("email", "jane@example.com")
+                .setIssuedAt(now)
+                .compact();
+        return jwtToken;
     }
 
 }
--- src/main/java/com/me/dao/UserDao.java
+++ src/main/java/com/me/dao/UserDao.java
@@ -8,7 +8,6 @@ package com.me.dao;
 import com.me.pojo.User;
 import java.util.ArrayList;
 import org.hibernate.HibernateError;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -46,24 +45,51 @@ public class UserDao {
         getSession().close();
     }
 
-    public User getUser(String username) {
+    public User getEmail(String email) {
         User user = null;
         try {
             beginTransaction();
-            Query q = getSession().createQuery("from User where username = '"+ username +"'");
+            Query q = getSession().createQuery("from User where email = '" + email + "'");
             ArrayList<User> userList = (ArrayList<User>) q.list();
-            if(userList.size() == 0 )
-            {
+            if (userList.isEmpty()) {
                 return null;
             }
             user = userList.get(0);
             commit();
         } catch (HibernateError e) {
-            e.printStackTrace();
             rollback();
         } finally {
             close();
         }
         return user;
+    }
+
+    public User getUser(String username) {
+        User user = null;
+        try {
+            beginTransaction();
+            Query q = getSession().createQuery("from User where username = '" + username + "'");
+            ArrayList<User> userList = (ArrayList<User>) q.list();
+            if (userList.isEmpty()) {
+                return null;
+            }
+            user = userList.get(0);
+            commit();
+        } catch (HibernateError e) {
+            rollback();
+        } finally {
+            close();
+        }
+        return user;
+    }
+    
+    public boolean createUser(String username,String password,String role,String email) {
+        User user = new User();
+        user.setEmail(email);
+        user.setRole("user");
+        user.setUsername(username);
+        user.setPassword(password);
+        session.save(user);
+        return true;
     }
 }
